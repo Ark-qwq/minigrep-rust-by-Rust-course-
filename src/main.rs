d5296db565986c07d4a632e1b92837fbb2a6890e
Ark-qwq <wfz0413@outlook.com>
2024-12-05 16:42:32 +0800

    second stage

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,52 @@
 use std::env;
 use std::fs;
+use std::process;
+
+
 fn main(){
     let args: Vec<String> = env::args().collect();
     // dbg!(&args);
 
-    let query = &args[1];
-    let file_path = &args[2];
-    
-    println!("Searching for {}", query);
-    println!("In file {}", file_path);
+    // let query = &args[1];
+    // let file_path = &args[2];
+    // let config:Config= parse_config(&args);
+    // let config:Config = Config::new(&args);
+    let config:Config = Config::build(&args).unwrap_or_else(|err|{
+        println!("Problem parsing arguments: {}",err);
+        process::exit(1);
+    });
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
 
-    let contents = fs::read_to_string(file_path)
+    let contents = fs::read_to_string(config.file_path)
         .expect("Should have been able to read the file");
     println!("With text:\n {contents}");
 } 
+struct Config{
+    query: String,
+    file_path: String,
+}
+// fn parse_config(args:&[String]) -> Config{
+//     let query:String = args[1].clone();
+//     let file_path:String= args[2].clone();
+//     return Config{query,file_path};
+// }
+
+impl Config{
+    fn new(args:&[String]) -> Config{
+        if args.len() < 3{
+            panic!("Invalid number of arguments");
+        }
+        let query:String = args[1].clone();
+        let file_path:String= args[2].clone();
+        return Config{query,file_path};
+    }
+    fn build(args:&[String]) -> Result<Config,&'static str>{
+        if args.len() < 3{
+            return Err("Invalid number of arguments");
+        }
+        let query:String = args[1].clone();
+        let file_path:String= args[2].clone();
+        return Ok(Config{query,file_path});
+    }
+}
